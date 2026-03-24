e252e6e1c73bf1a985ad28736ce42c083836a119
sofiapnm <sofia.pinedomontarelo@stud.hslu.ch>
2026-03-24 11:31:02 +0000

    Pareto implementation, missing battery data

--- AEM Python Sim/V1.2 Pareto Front/V1.1 Abstraction (Gurobi)/parameters/general.py
+++ AEM Python Sim/V1.2 Pareto Front/V1.1 Abstraction (Gurobi)/parameters/general.py
@@ -7,5 +7,5 @@ GENERAL = {
     "solver_name": "gurobi",
     "gurobi_output_flag": 0,
     "pareto_num_points": 50,
-    "emissions_penalty": 120.0, #CHF/ton CO2eq.
+    "emissions_penalty": 0.0, #120 CHF/ton CO2eq. for thermal fuels, need to find renewable elec generation tax (if any)
 }
